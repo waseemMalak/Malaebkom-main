9d3b8a2a44399c661ace6bd37b2ed53c3ac8864b
waseemMalak <malakwaseem98@gmail.com>
2023-05-04 20:19:19 +0300

    add new field Opening hours , field creator id, services

--- lib/addNewField.dart
+++ lib/addNewField.dart
@@ -17,16 +17,30 @@ class _FieldOwnerFormState extends State<FieldOwnerForm> {
   final _locationController = TextEditingController();
   final _fieldImagesController = TextEditingController();
   final _fieldServicesController = TextEditingController();
-  final _fieldOwnerIdController = TextEditingController();
+
   final _openingHoursController = TextEditingController();
+  TimeOfDay _openingHoursStart = TimeOfDay(hour: 8, minute: 0);
+  TimeOfDay _openingHoursEnd = TimeOfDay(hour: 20, minute: 0);
+
+  final List<String> _services = [
+    'Football',
+    'Water',
+    'Toilets',
+  ];
+  String _selectedService = '';
 
   @override
+  void initState() {
+    super.initState();
+    _selectedService = _services.isNotEmpty ? _services[0] : '';
+  }
+
   void dispose() {
     _fieldNameController.dispose();
     _locationController.dispose();
     _fieldImagesController.dispose();
     _fieldServicesController.dispose();
-    _fieldOwnerIdController.dispose();
+
     _openingHoursController.dispose();
     super.dispose();
   }
@@ -81,41 +95,74 @@ class _FieldOwnerFormState extends State<FieldOwnerForm> {
                     return null;
                   },
                 ),
-                TextFormField(
-                  controller: _fieldServicesController,
+                // TextFormField(
+                //   controller: _fieldServicesController,
+                //   decoration: InputDecoration(
+                //     labelText: 'Field Services',
+                //   ),
+                //   validator: (value) {
+                //     if (value!.isEmpty) {
+                //       return 'Please enter at least one service';
+                //     }
+                //     return null;
+                //   },
+                // ),
+                DropdownButtonFormField<String>(
+                  value: _selectedService,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedService = value!;
+                    });
+                  },
+                  items: _services
+                      .map((service) => DropdownMenuItem(
+                            value: service,
+                            child: Text(service),
+                          ))
+                      .toList(),
                   decoration: InputDecoration(
                     labelText: 'Field Services',
                   ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter at least one service';
-                    }
-                    return null;
-                  },
                 ),
-                TextFormField(
-                  controller: _fieldOwnerIdController,
-                  decoration: InputDecoration(
-                    labelText: 'Field Owner ID',
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter a field owner ID';
-                    }
-                    return null;
-                  },
+
+                SizedBox(
+                  height: 5,
                 ),
-                TextFormField(
-                  controller: _openingHoursController,
-                  decoration: InputDecoration(
-                    labelText: 'Opening Hours',
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter opening hours';
-                    }
-                    return null;
-                  },
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Opening Hours"),
+                    SizedBox(
+                      height: 5,
+                      width: 5,
+                    ),
+                    GestureDetector(
+                      onTap: () => _selectOpeningHoursStart(context),
+                      child: Container(
+                        padding: const EdgeInsets.all(12.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(4.0),
+                        ),
+                        child: Text(
+                          'From: ${_formatTimeOfDay(_openingHoursStart)}',
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () => _selectOpeningHoursEnd(context),
+                      child: Container(
+                        padding: const EdgeInsets.all(12.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(4.0),
+                        ),
+                        child: Text(
+                          'To: ${_formatTimeOfDay(_openingHoursEnd)}',
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(height: 16.0),
                 ElevatedButton(
@@ -134,15 +181,51 @@ class _FieldOwnerFormState extends State<FieldOwnerForm> {
     );
   }
 
+  void _selectOpeningHoursStart(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: _openingHoursStart,
+    );
+    if (picked != null && picked != _openingHoursStart) {
+      setState(() {
+        _openingHoursStart = picked;
+      });
+    }
+  }
+
+  void _selectOpeningHoursEnd(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: _openingHoursEnd,
+    );
+    if (picked != null && picked != _openingHoursEnd) {
+      setState(() {
+        _openingHoursEnd = picked;
+      });
+    }
+  }
+
+  String _formatTimeOfDay(TimeOfDay timeOfDay) {
+    String hour = (timeOfDay.hour % 12).toString().padLeft(2, '0');
+    String minute = timeOfDay.minute.toString().padLeft(2, '0');
+    String period = timeOfDay.hour < 12 ? 'AM' : 'PM';
+    return '$hour:$minute $period';
+  }
+
   void _addField() async {
     try {
+      String userId = FirebaseAuth.instance.currentUser!.uid;
+      DocumentReference fieldRef =
+          FirebaseFirestore.instance.collection('fields').doc();
       await FirebaseFirestore.instance.collection('fields').add({
         'fieldName': _fieldNameController.text,
         'location': _locationController.text,
         'fieldImages': _fieldImagesController.text.split(','),
-        'fieldServices': _fieldServicesController.text.split(','),
-        'fieldOwnerId': _fieldOwnerIdController.text,
-        'openingHours': _openingHoursController.text,
+        // 'fieldServices': _fieldServicesController.text.split(','),
+        'fieldServices': _selectedService.split(','),
+        'openingHours':
+            '${_formatTimeOfDay(_openingHoursStart)} - ${_formatTimeOfDay(_openingHoursEnd)}',
+        'userId': userId,
       });
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Field added successfully')),
